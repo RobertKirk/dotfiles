4fc1077d7f8d7faa8dc3bc7279d26a7fab75118f
robertkirk <robert.kirk@smarkets.com>
2019-02-25 10:51:25 +0000

    some cleaning, new things on switching to sakura (for now)
    tmux zsh config

--- install_tools.sh
+++ install_tools.sh
@@ -1,12 +1,12 @@
 sudo apt-get update
-sudo apt-get install vim zsh spotifycli gnome-tweaks tmux curl wget wmctrl git ssh openssh
+sudo apt-get install vim zsh spotifycli gnome-tweaks tmux curl wget wmctrl git ssh openssh sakura tizonia lua5.3 psql
 
-# vscode, spotify, firefox, wavebox, oh-my-zsh, fonts, anaconda, python latest, z.lua, lua5.3
+# vscode, spotify, firefox, wavebox, oh-my-zsh, fonts, anaconda, python latest, z.lua
 
 cd ~
 mkdir repos
 touch .vimrc
 touch .zshrc
 
-# vscode plugins: vim, git, python, base16themes, gitlens, rest client, bracket pair colourizer, vscode-spotify
+# vscode plugins: vim, git, python, base16themes, gitlens, bracket pair colourizer 2, vscode-spotify, material icon theme zenburn
 

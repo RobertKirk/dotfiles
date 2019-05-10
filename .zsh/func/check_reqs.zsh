920a6cef41eb4e14e480b2541311f3f49fe68fe3
Robert Kirk <robert.kirk.3.14@gmail.com>
2019-05-10 08:16:55 +0100

    adding scrnsht using import, small linting thing

--- .zsh/func/check_reqs.zsh
+++ .zsh/func/check_reqs.zsh
@@ -1,5 +1,5 @@
 function() check_reqs() {
-  for line in $(cat ~/requirements.txt ); do
+  for line in $(cat ~/requirements.txt); do
     which $line > /dev/null
     if [ $? -ne 0 ]; then
       echo no $line

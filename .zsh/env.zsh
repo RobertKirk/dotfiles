ff708373d0964b926d5ae008588f43141f9af1ee
Robert Kirk <robert.kirk@smarkets.com>
2019-05-18 23:39:54 +0100

    adding some nnn stuff, correcting advanced cp move aliases, trayer for kdeconnect things

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,3 +1,5 @@
 export EDITOR='nvim'
 export VISUAL=$EDITOR
 export PAGER=less
+export NNN_CP_MV_PROG=1
+export NNN_USE_EDITOR=1
---
+++ .zsh/func/n.zsh
@@ -0,0 +1,11 @@
+export NNN_TMPFILE="/tmp/nnn"
+
+n()
+{
+        nnn "$@"
+
+        if [ -f $NNN_TMPFILE ]; then
+                . $NNN_TMPFILE
+                rm $NNN_TMPFILE
+        fi
+}

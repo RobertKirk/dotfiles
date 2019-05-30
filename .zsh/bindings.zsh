b1631d1972046009e6fffbc04c21ae31b6205640
Robert Kirk <robert.kirk@smarkets.com>
2019-05-30 16:35:57 +0100

    adding magic enter to show git status on empty enter line

--- .zsh/bindings.zsh
+++ .zsh/bindings.zsh
@@ -9,3 +9,19 @@ bindkey -M vicmd '^V' edit-command-line
 bindkey -M vicmd '^K' history-substring-search-up
 
 bindkey '^K' history-substring-search-up
+
+# Detect empty enter, execute git status if in git dir
+magic-enter () {
+  if [[ -z $BUFFER ]]; then
+    if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+      echo -ne '\n'
+      git status -sb
+    fi
+    zle accept-line
+  else
+    zle accept-line
+  fi
+}
+
+zle -N magic-enter
+bindkey "^M" magic-enter

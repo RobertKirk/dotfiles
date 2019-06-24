05d9626af7eb010b7f2dae66e8efe052373981c6
Robert Kirk <robert.kirk@smarkets.com>
2019-06-24 12:31:49 +0100

    making completions a bit nicer looking, adding kitty and i3, changing to kitty

---
+++ .zsh/completion.zsh
@@ -0,0 +1,26 @@
+autoload -Uz colors; colors
+LS_COLORS='di=01;36:ln=01;35:ex=01;31:'
+LS_COLORS+='*.c=01;35:*.cpp=01;35:*.js=01;35:*.json=01;35:*.hs=01;35:*.py=01;35:*.pl=01;35:'
+LS_COLORS+='*.tex=01;35:*.csv=01;35:*.r=01;35:*.R=01;35:*.txt=01;35:*.sty=01;35:*.coffee=01;35:*.class=01;35:*.java=01;35:*.less=01;35:*.css=01;35:'
+LS_COLORS+='*.jpg=01;33:*.png=01;33:*.bmp=01;33:*.JPG=01;33:*.PNG=01;33:*.BMP=01;33:'
+LS_COLORS+='*.gz=01;34:*.tar=01;34:*.zip=01;34:'
+LS_COLORS+='*.pdf=01;32:*makefile=01;32:*.html=01;32:'
+export LS_COLORS
+
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=253,bold"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=40
+
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path $ZDOTDIR/completion-cache
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+
+zstyle ':completion:*:corrections' format '%U%F{green}%d (errors: %e)%f%u'
+zstyle ':completion:*:warnings' format '%F{202}%BNo matches for: %F{214}%d%b'
+zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /home/robert/.local/bin
+setopt interactivecomments
+
+zmodload zsh/complist

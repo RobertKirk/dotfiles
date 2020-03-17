3c2fc8e18a9a01562f7db629a1e3c0b5066bfc72
Robert Kirk <robert.kirk.3.14@gmail.com>
2020-03-17 10:51:09 +0000

    zsh: Adding remote clipboard functionality

---
+++ .zsh/func/remote_clip.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+SSH_REMOTE_CLIPBOARD_PORT=5556
+
+remote_clip_daemon() {
+  while (true); do echo "Waiting..." ;  nc -l $SSH_REMOTE_CLIPBOARD_PORT | xclip; echo "Copied: "; xclip -o | sed 's/^/  /'; done
+}
+
+start_clip_daemon() {
+  remote_clip_daemon &> /var/log/remote_clip_daemon.log
+}
+
+xclip() {
+  if (printenv SSH_CLIENT > /dev/null); then
+    nc -q0 localhost $SSH_REMOTE_CLIPBOARD_PORT "$@"
+  else
+    command xclip "$@"
+  fi
+}

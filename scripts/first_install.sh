02a029193077a4bce698d2c78310a254be2fa0c7
Robert Kirk <robert.kirk.3.14@gmail.com>
2020-03-30 08:31:12 +0000

    aliases, first install: Adding fd, rg, lua, etc.

--- scripts/first_install.sh
+++ scripts/first_install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install zsh tmux
+sudo apt-get install zsh tmux lua5.3
 
 # zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
@@ -11,4 +11,22 @@ chmod u+x nvim.appimage
 mv nvim.appimage "$HOME/.local/bin/nvim"
 nvim -c 'PlugInstall' -c qa
 
+# ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+
+# fd
+curl -LO https://github.com/sharkdp/fd/releases/download/v7.5.0/fd_7.5.0_amd64.deb
+sudo dpkg -i fd_7.5.0_amd64.deb
+
 git submodule init
+git submodule update
+
+# fzf
+echo y y n | ./.fzf/install.sh
+
+# pynvim
+pip install --user pynvim neovim
+pip3 install --user pynvim neovim
+
+nvim -c 'UpdateRemotePlugins' -c qa

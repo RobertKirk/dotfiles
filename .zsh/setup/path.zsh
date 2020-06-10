f76eee5d34825a821065f47dd8ce692e5f0c4d13
Robert Kirk <robert.kirk.3.14@gmail.com>
2020-06-10 12:32:43 +0100

    spicetify: Adjusting config and adding to path

--- .zsh/setup/path.zsh
+++ .zsh/setup/path.zsh
@@ -1,5 +1,6 @@
-# local, yarn, npm, cargo, ruby bins
-PATH="$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.cargo/bin:$HOME/.rbenv/bin:$PATH"
+# local, yarn, npm, cargo, ruby bins, spicetify-cli
+export SPICETIFY_INSTALL="$HOME/spicetify-cli"
+PATH="$HOME/.local/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.cargo/bin:$HOME/.rbenv/bin:$SPICETIFY_INSTALL:$PATH"
 # pyenv bins
 PYENV_ROOT="$HOME/repos/pyenv" 
 PATH="$PYENV_ROOT/bin:$PATH"
@@ -9,3 +10,5 @@ eval "$(rbenv init -)"
 export GEM_HOME="$HOME/gems"
 export PATH="$HOME/gems/bin:$PATH"
 export fpath=( $HOME/.zsh/functions $fpath )
+
+export PATH="$SPICETIFY_INSTALL:$PATH"

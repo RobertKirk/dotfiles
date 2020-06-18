9f2f85bca315a74e6831f22a1da6674c769a2d35
Robert Kirk <robert.kirk.3.14@gmail.com>
2020-06-18 09:33:19 +0100

    aliases: adding calc alias

--- .aliases.sh
+++ .aliases.sh
@@ -98,6 +98,10 @@ command_freq() {
 
 getkeycods() { xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }' }
 
+calc() {
+  printf "%s\n" "$*" | bc
+}
+
 # single letter aliases:
 # alias a=
 alias b='buku --suggest'

c3e5c5ba32fb96ebb40475527447d8580f5f12aa
Robert Kirk <robert.kirk.3.14@gmail.com>
2021-01-12 10:17:03 +0000

    scripts: Adjusting rsync-changes for optional tails proxy

--- scripts/rsync-changes.sh
+++ scripts/rsync-changes.sh
@@ -1,21 +1,28 @@
 #!/bin/bash -l
 ##
 ## Keeps the remote synced to the current folder changes
-##
+## Initialises remote folder at startup
 
-DEST="$1" # remote dest eg username@beaker.cs.ucl.ac.uk:~/dir1/dir2
-initialise="$2"
+DEST="$1" # remote dest eg username@beaker.cs.ucl.ac.uk:~/dir1/dir2, or beaker:~/
+via_tails="$2"
+no_initialise="$3"
 # EXCLUDES={'.git', '*/.git', '*.git', '__pycache__','*/__pycache__','wandb','logs'}
+
+if [ $via_tails ]; then
+  ssh_command='ssh -J tails'
+else
+  ssh_command='ssh'
+fi
 
 if [ -z "$DEST" ]; then exit 1; fi
 echo dest= $DEST
 
-if [ "$initialise" ];
+if [ "$no_initialise" ];
 then
   echo "Not Initialising"
 else
   echo "Initialising"
-  rsync -uazv --progress --exclude-from='rsync_excludes.txt' -e 'ssh -J tails' * ${DEST}/
+  rsync -uazv --progress --exclude-from='rsync_excludes.txt' -e "$ssh_command" * ${DEST}/
   echo "Initialisation Complete!"
 fi
 
@@ -25,7 +32,7 @@ while read file
 do
         relpath=${file#$(pwd)/}
         echo rsyncing $relpath
-        rsync -uazv --exclude-from='rsync_excludes.txt' -e 'ssh -J tails' $relpath ${DEST}/$relpath
+        rsync -uazv --exclude-from='rsync_excludes.txt' -e "$ssh_command" $relpath ${DEST}/$relpath
         the_date=$(date)
         echo  "rsync $relpath complete at $the_date"
         echo

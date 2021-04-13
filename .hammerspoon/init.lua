2381f2bf426d993a5e0f1f1afc47fff74a4ea4c0
Robert Kirk <robert.kirk.3.14@gmail.com>
2021-04-13 17:24:27 +0100

    hammerspoon: Adjust for laptop, layout saving
    First crack at layout saving, currently a WIP

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -9,6 +9,12 @@ hs.alert.show("Config Reloaded")
 -- Laptop/Desktop Mode{{{
 laptopMode = #hs.screen.allScreens() == 1
 
+if laptopMode then
+  two_modifiers = {"option", "cmd"}
+else
+  two_modifiers = {"ctrl", "cmd"}
+end
+
 -- }}}
 -- Scrolling for Volume{{{
 hs.loadSpoon("VolumeScroll")
@@ -25,10 +31,14 @@ mapping = {
   t = "Alacritty",
   m = "Spotify",
   r = "Roam Research",
-  s = "Skim"
+  s = "Skim",
+  z = "zoom.us",
+  a = "Calendar (Google)",
+  n = "Notion",
+  b = "Zotero",
 }
 
-spoon.AppLauncher.modifiers = {"ctrl", "cmd"}
+spoon.AppLauncher.modifiers = two_modifiers
 
 spoon.AppLauncher:bindHotkeys(mapping)
 
@@ -38,14 +48,86 @@ spoon.AppLauncher:bindHotkeys(mapping)
 hs.loadSpoon("TextClipboardHistory")
 spoon.TextClipboardHistory:start()
 
-hs.hotkey.bind({"cmd", "ctrl"}, "C", function()
+hs.hotkey.bind(two_modifiers, "C", function()
   spoon.TextClipboardHistory:toggleClipboard()
 end)
 
 -- }}}
+-- Layouts {{{
+
+allLayouts = {}
+
+function save_layout ()
+  all_windows = hs.window.allWindows()
+  saved_layout = {}
+  for i, window in pairs(all_windows) do
+    print(window:application():name(), window:screen():name(), window:frame()) 
+    table.insert(
+      saved_layout,
+      {window:application():name(), nil, window:screen():name(), window:frame():toUnitRect(window:screen():frame()), nil, nil}
+    )
+  end
+  table.insert(allLayouts, saved_layout)
+  return saved_layout
+end
+
+
+function serializeTable(val, name, skipnewlines, depth)
+    skipnewlines = skipnewlines or false
+    depth = depth or 0
+
+    local tmp = string.rep(" ", depth)
+
+    if name then tmp = tmp .. name .. " = " end
+
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+
+        for k, v in pairs(val) do
+            tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+        end
+
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+
+    print(tmp)
+    return tmp
+end
+
+layoutChooser = hs.chooser.new(function(i)
+    hs.layout.apply(allLayouts[i["uuid"]])
+  end
+)
+layoutChooser:choices(
+  function()
+    choices = {}
+    for i, layout in pairs(allLayouts) do
+      choices[i] = {}
+      choices[i]["text"] = serializeTable(layout)
+      choices[i]["uuid"] = i
+    end
+    print(choices)
+    return choices
+  end
+)
+
+function appply_layout ()
+  layoutChooser:show()
+end
+
+-- }}}
 -- Window arrangement{{{
+--
 -- Left Half
-hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
+hs.hotkey.bind(two_modifiers, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -59,7 +141,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
 end)
 
 -- Right Half
-hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
+hs.hotkey.bind(two_modifiers, "L", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -73,7 +155,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
 end)
 
 -- Maximise
-hs.hotkey.bind({"cmd", "ctrl"}, "K", function()
+hs.hotkey.bind(two_modifiers, "K", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
@@ -87,7 +169,7 @@ hs.hotkey.bind({"cmd", "ctrl"}, "K", function()
 end)
 
 -- Small Centred
-hs.hotkey.bind({"cmd", "ctrl"}, "J", function()
+hs.hotkey.bind(two_modifiers, "J", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
